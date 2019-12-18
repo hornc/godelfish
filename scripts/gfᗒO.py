d8920b6864fb781528d87b8998a163544bb168f1
Charles Horn <charles.horn@gmail.com>
2019-12-19 11:52:41 +1300

    add passing tests for original I() code

--- scripts/gfᗒO.py
+++ scripts/gfᗒO.py
@@ -7,19 +7,19 @@ from math import floor, log
 
 
 # Final Output of a Gödelfish program, excludes the current accumulator value.
-O = lambda 𝜑̈, r=10, d=4: floor(E(𝜑̈, r, d)/r**d)
+O = lambda 𝜑̈, r=10, d=4: floor(E(𝜑̈, r**d)/r**d)
 
 
-def I(o, c, r=10, d=4):
+def I(o, c, z=10**4):
     # o = current output (int) 
     # c = command (int)
     # temp prevent negative:
     if o == 0 and c == 0:
         return 0
     #print('o: %d c: %d' % (o, c))
-    ac = o - (o//(r**d))*r**d
+    ac = o - (o//(z))*z
     if c == 3:  # output
-       return o * r**d - o + ac
+       return o * z - o + ac
 
     if c == 2:  # square
        v = ac**2 - ac
@@ -35,14 +35,46 @@ def I(o, c, r=10, d=4):
         return v
 
 
-def E(𝜑̈, r=10, d=4):
+def E(𝜑̈, z=10**4):
     """
     Evaluate a Gödelfish program. Returns full state:
     current accumulator and all previous output.
     """
     if 𝜑̈ == 0:
         return 0
-    return sum([I(E(floor(𝜑̈/(4**(floor(log(𝜑̈, 4) - i + 2)))), r, d), floor(𝜑̈/4**(floor(log(𝜑̈, 4)) - i + 1)) - (4 * floor(𝜑̈/(4**((floor(log(𝜑̈, 4)) - i + 2))))), r, d) for i in range(floor(log(𝜑̈, 4)) + 2)])
+    return sum([I(E(floor(𝜑̈/(4**(floor(log(𝜑̈, 4) - i + 2)))), z), floor(𝜑̈/4**(floor(log(𝜑̈, 4)) - i + 1)) - (4 * floor(𝜑̈/(4**((floor(log(𝜑̈, 4)) - i + 2))))), z) for i in range(floor(log(𝜑̈, 4)) + 2)])
+    #return sum([v(E(s(𝜑̈, i, 2), z) % z, d(E(s(𝜑̈, i, 2), z), c(𝜑̈, i), z)) for i in range(floor(log(𝜑̈, 4)) + 2)])
+
+def c(𝜑̈, i):
+    """ c: current Command."""
+    return s(𝜑̈, i, 0) - 4 * s(𝜑̈, i, 1)
+
+def d(x, c, z):
+    """ d: Difference."""
+    if c == 0:
+        return -1
+    elif c == 1:
+        return 1
+    elif c == 2:
+        return (x%z)**2 - x%z
+    elif c == 3:
+        return x * (z - 1)
+
+def s(𝜑̈, i, n):
+    """ s: current State."""
+    return floor(𝜑̈ / 4**(floor(log(𝜑̈, 4)) - i + n))
+
+def v(a, x):
+    """ v: oVerflow check.
+        a = current accumulator
+        x = proposed difference
+    """
+    if a + x < 0:
+        return -a
+    elif a + x == 256:
+        return -a
+    else:
+        return a + x
 
 
 if __name__ == '__main__':
---
+++ scripts/tests/test_gfᗒO.py
@@ -0,0 +1,72 @@
+import pytest
+import gfᗒO
+
+
+def test_commands():
+    """ Test the individual commands are read correctly."""
+    b4 = '12302'  # Gödelfish in base4
+    gf = int(b4, 4)
+    for i,c in enumerate(b4):
+        assert gfᗒO.c(gf, i) == int(c, 4)
+
+
+# Gödelfish programs in base4
+cases =[
+        ('1', 0),
+        ('13', 1),
+        ('131', 1),
+        ('113', 2),
+        ('1313', 10002),  # Multiple outputs
+        ('123', 1),  # Squaring
+        ('1123', 4),
+        ('11123', 9),
+        ('112311003', 40004),
+        ('1123123', 40025),
+        ]
+
+
+@pytest.mark.parametrize('prog,expect', cases)
+def test_basic_commands(prog, expect):
+    radix = 10
+    digits = 4
+    assert gfᗒO.O(int(prog, 4), radix, digits) == expect
+
+
+def test_no_negatives():
+    gf = '100000000000000113'
+    assert gfᗒO.O(int(gf, 4)) == 2
+
+
+def test_subtraction():
+    gf = '11213030303030'
+    assert gfᗒO.O(int(gf, 4), 10, 2) == 504030201
+    assert gfᗒO.O(int(gf, 4), 10, 3) == 5004003002001
+    #assert gfᗒO.O(int(gf, 4), 10, 4) == 500040003000200010000
+
+def test_mandatory_arithmetic_zero():
+    gf = int('112223', 4)
+    assert gfᗒO.O(gf) == 0
+
+
+def test_mandatory_arithmetic_288():
+    gf = int('011221203', 4)
+    assert gfᗒO.O(gf) == 288
+
+
+@pytest.mark.skip("This takes too long to compute (with the current code).")
+def test_mandatory_arithmetic_long_zero():
+    gf = int('1122120000000000000000000000000000000003', 4)
+    assert gfᗒO.O(gf, 16, 2) == 0
+
+
+def test_ASCII_output_in_different_bases():
+    # HI = 072, 073 decimal
+    # HI = 0x48, 0x49 hex
+    gf = int('1121111211111111313', 4)
+    dec_ = gfᗒO.O(gf, 10, 4)
+    hex_ = gfᗒO.O(gf, 16, 2)
+    assert dec_ != hex_
+    assert dec_ == 720073
+    assert hex_ == 0x4849
+
+
